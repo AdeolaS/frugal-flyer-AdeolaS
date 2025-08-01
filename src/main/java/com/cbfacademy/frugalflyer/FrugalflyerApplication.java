9dba17104feeaabb74656b8cc64b58ab9391c5d4
AdeolaS <adeoladesign@hotmail.co.uk>
2025-08-01 21:16:30 +0100

    Project initialised and running

---
+++ src/main/java/com/cbfacademy/frugalflyer/FrugalflyerApplication.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.frugalflyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrugalflyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrugalflyerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbfacademy/frugalflyer/FrugalflyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.frugalflyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrugalflyerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

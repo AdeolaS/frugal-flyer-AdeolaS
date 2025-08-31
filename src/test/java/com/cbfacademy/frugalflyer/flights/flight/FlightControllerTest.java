da3921157a91594ad5df8bf9a78d957a67d9a36d
AdeolaS <adeoladesign@hotmail.co.uk>
2025-08-31 22:45:29 +0100

    Still struggling to get Flight controller tests working

--- src/test/java/com/cbfacademy/frugalflyer/flights/flight/FlightControllerTest.java
+++ src/test/java/com/cbfacademy/frugalflyer/flights/flight/FlightControllerTest.java
@@ -16,11 +16,11 @@ import org.springframework.context.annotation.Description;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import com.cbfacademy.frugalflyer.flights.airport.Airport;
-
+import com.cbfacademy.frugalflyer.flights.customExceptions.InvalidDateException;
+import com.cbfacademy.frugalflyer.flights.destination.Destination;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -33,9 +33,11 @@ public class FlightControllerTest {
 	@MockitoBean
 	private FlightService flightService;
 
-    private Airport london = new Airport();
-    private Airport newYork = new Airport();
-    private Airport barcelona = new Airport();
+    private Destination destination = new Destination("City", "Country", "Climate", List.of("tag"), List.of());
+    private Airport london = new Airport("LHR", "Heathrow Airport", destination);
+    private Airport newYork = new Airport("JFK", "John F. Kennedy International Airport", destination);
+    private Airport barcelona = new Airport("BCN", "Barcelona El Prat Airport", destination);
+    private Flight flight = new Flight(london, newYork, 700, LocalDate.now());
 
     private List<Flight> defaultFlights = new ArrayList<>() {
         {
@@ -46,80 +48,81 @@ public class FlightControllerTest {
     };
 
     @Test
-    @Description("GET /api/flights/search-via-airport returns matching flights")
-    void test_searchFlightsUsingArrivalAirport() throws Exception {
+    @Description("GET /api/flights/search-via-airport returns 200 when departure airport is given")
+    void test_searchFlightsUsingArrivalAirport_depatureAirportGiven() throws Exception {
+        
         // Arrange
         when(flightService.searchFlightsUsingArrivalAirport(anyDouble(),anyString(),anyString(),any(),any())).thenReturn(defaultFlights);
 
-        this.mockMvc.perform(get("/search-via-airport")).andDo(print())
+        //perform() method will call the GET request, and the request
+        this.mockMvc.perform(get("/api/flights/search-via-airport")
+        .param("departureAirport", "LHR"))
+        // and response will be printed
+        .andDo(print())
+        //assert that the response will be OK 200
         .andExpect(status().isOk());
-        //.andExpect(content().equals(defaultFlights));
+    }
 
-
-        // this.mockMvc.perform(get("/search-via-airport")).andDo(print()).andExpect(status().isOk())
-        // .andExpect(content().equals(defaultFlights));
+    @Test
+    @Description("GET /api/flights/search-via-airport returns 400 when InvalidDateException is thrown")
+    void test_searchFlightsUsingArrivalAirport_InvalidDateException() throws Exception {
         
-        //(get("/search-via-airport")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("Hello, Mock")));
+        // Arrange
+        when(flightService.searchFlightsUsingArrivalAirport(anyDouble(),anyString(),anyString(),any(LocalDate.class),any(Integer.class))).thenThrow(new InvalidDateException(null));
+
+        //perform() method will call the GET request, and the request
+        this.mockMvc.perform(
+        get("/api/flights/search-via-airport")
+        .param("departureAirport", "LHR")
+        .param("departureDate", "2024-01-01"))
+        // and response will be printed
+        .andDo(print())
+        //assert that the response will be BAD_REQUEST 400
+        .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Description("GET /api/flights/search-via-climate-and-tags returns 200 when departure airport is given")
+    void test_searchFlightsUsingClimateAndTags_depatureAirportGiven() throws Exception {
+        
+        // Arrange
+        when(flightService.searchFlightsUsingClimateAndTags(anyDouble(),anyString(),anyString(),any(), any(), any())).thenReturn(defaultFlights);
+
+        //perform() method will call the GET request, and the request
+        this.mockMvc.perform(get("/api/flights/search-via-climate-and-tags")
+        .param("departureAirport", "LHR"))
+        //assert that the response will be OK 200
+        .andExpect(status().isOk());
+    }
+
+    @Test
+    @Description("GET /surprise-me returns 200 when departure airport is given")
+    void test_findRandomFlight_depatureAirportGiven() throws Exception {
+        
+        // Arrange
+        when(flightService.findRandomFlight(anyString())).thenReturn(flight);
+
+        //perform() method will call the GET request, and the request
+        this.mockMvc.perform(get("/api/flights/surprise-me")
+        .param("departureAirport", "LHR"))
+        //assert that the response will be OK 200
+        .andExpect(status().isOk());
+    }
+
+    @Test
+    @Description("GET /api/flights/cheap-flights returns 200 when airports and threshold is given")
+    void test_findCheapFlightAnomalies_AirportsAndThresholdGiven() throws Exception {
+        
+        // Arrange
+        when(flightService.findCheapFlightAnomalies(anyString(), anyString(), anyDouble())).thenReturn(defaultFlights);
+
+        //perform() method will call the GET request, and the request
+        this.mockMvc.perform(get("/api/flights/cheap-flights")
+        .param("departureAirport", "LHR")
+        .param("arrivalAirport", "JFK")
+        .param("threshold", "0.5"))
+        //assert that the response will be OK 200
+        .andExpect(status().isOk());
     }
 
 }
-
-
-// @SpringBootTest(classes = FrugalflyerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// public class FlightControllerTest {
-    
-//     @LocalServerPort
-// 	private int port;
-
-// 	private URI baseURI;
-
-// 	@Autowired
-// 	private TestRestTemplate restTemplate;
-
-//     private Airport london = new Airport();
-//     private Airport newYork = new Airport();
-//     private Airport barcelona = new Airport();
-
-//     private List<Flight> defaultFlights = new ArrayList<>() {
-//         {
-//             add(new Flight(london, newYork, 700, LocalDate.now()));
-//             add(new Flight(london, barcelona, 149.99, LocalDate.now()));
-//             add(new Flight(london, barcelona, 199.99, LocalDate.now()));
-//         }
-//     };
-
-//     @MockBean
-//     private FlightService flightService;
-
-//     @BeforeEach
-// 	void setUp() throws RuntimeException {
-// 		this.baseURI = UriComponentsBuilder.newInstance()
-// 				.scheme("http")
-// 				.host("localhost")
-// 				.port(port)
-// 				.path("api/flights")
-// 				.build()
-// 				.toUri();
-// 	}
-
-//     @Test
-// 	@Description("GET /api/flights/search-via-airport returns matching flights")
-// 	void test_searchFlightsUsingArrivalAirport() throws Exception {
-
-//         // Arrange
-//         when(flightService.searchFlightsUsingArrivalAirport(anyDouble(),anyString(),anyString(),any(),any())).thenReturn(defaultFlights);
-
-// 		// Act
-// 		ResponseEntity<List<Flight>> response = restTemplate.exchange(baseURI, HttpMethod.GET, null,
-// 				new ParameterizedTypeReference<List<Flight>>() {}
-//                 );
-
-//         List<Flight> responseFlights = response.getBody();
-
-// 		// Assert
-// 		assertEquals(HttpStatus.OK, response.getStatusCode());
-// 		assertNotNull(responseFlights);
-// 		assertEquals(defaultFlights.size(), responseFlights.size());
-// 		verify(flightService).searchFlightsUsingArrivalAirport(anyDouble(),anyString(),anyString(),any(),any());
-// 	}
-// }
